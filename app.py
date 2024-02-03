c3bc622917c89e6dd5545c4d93e95ecfc7f1932e
anshul rawat <anshulrawat541@gmail.com>
2024-02-03 16:40:45 +0530

    !

--- app.py
+++ app.py
@@ -25,9 +25,6 @@ def get_summary(transcript):
     summariser = pipeline('summarization',model="facebook/bart-large-cnn")
     summary = ""
     t = 2
-    # for i in range(0, (len(transcript)//1000)+1):
-    #     summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
-    #     summary = summary + summary_text + ' '
     if transcript != "":
         while t>0:
                 chunks = textwrap.wrap(transcript, 800)
